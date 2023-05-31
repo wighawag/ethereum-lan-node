fc9bda7b885003bbd54d8a2e91ef7265cd890f53
wighawag <wighawag@gmail.com>
2023-05-31 10:14:16 +0100

    hidden volume

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 if [[ "$1" == "with-explorer" ]]
 then
-docker compose -f docker-compose.yml -f docker-compose.blockscout.yml up
+echo "currently not supported"
+exit 1
+docker compose -f docker-compose.yml -f docker-compose.blockscout.yml up -d
 else
-docker compose up
+if [ -z "$1" ]
+then
+docker compose up -d
+else
+echo "do not accept any argument"
+exit 1
+fi
 fi
