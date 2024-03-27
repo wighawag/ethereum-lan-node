121d17535ccee82762c3bea9c2d5947a2a7208da
wighawag <wighawag@gmail.com>
2024-03-27 15:10:40 +0000

    install on reboot

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+cp install/etc/systemd/system/lan-blockscout.service /etc/systemd/system/lan-blockscout.service
+cp install/etc/systemd/system/lan-ethereum.service /etc/systemd/system/lan-ethereum.service
